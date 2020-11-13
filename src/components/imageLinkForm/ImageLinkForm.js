f65a03a50ea386f73b9628eaa64bed0961fd6865
Apurba Panja <apurbapanja1@gmail.com>
2020-11-13 14:56:33 +0530

    Added four new UI components to the app
    1. Navigation Component for sing in / sign out
    2. Logo components contains logo
    3. ImageLinkFrom component contains image link input field and submit(detect) button
    4. Rank component conatain information about user ranks

---
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p>{'This magic brain will detect faces in your picture. Give it a try.'}</p>
+      <div className="center">
+        <div className='center form pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='text' />
+          <button className='w-30 grow f4 link pv2 dib white bg-light-purple' type='button'>
+            {'Detect'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;
