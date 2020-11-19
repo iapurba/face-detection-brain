d778196c99bc109786421e1db8026959138cb173
Apurba Panja <apurbapanja1@gmail.com>
2020-11-19 23:45:53 +0530

    Update App.js component, Add new methods
    Add calculateFaceLOcation and detectFaceBox methods to the App class.

--- src/App.js
+++ src/App.js
@@ -31,23 +31,41 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
+      box: {},
     }
   }
 
+  calculateFaceLoaction = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
+    this.setState({input: event.target.value});
     console.log(event.target.value);
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
-      function(response) {
-        console.log(response);
-      },
-      function(err) {
-
-      }
-    );
+    this.setState({imageUrl: this.state.input});
+    app.models.predict (
+      Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => this.displayFaceBox(this.calculateFaceLoaction(response)))
+      .catch(err => console.log(err));
   }
 
   render(){
@@ -61,7 +79,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceDetection />
+        <FaceDetection boundingBox={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
