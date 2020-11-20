6517e201c14287a24ae714b11b00353aa660b109
Apurba Panja <apurbapanja1@gmail.com>
2020-11-20 15:50:59 +0530

    Modify Navigation.js for sign in and out

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <nav style={{display:'flex', justifyContent:'flex-end'}}>
-      <p className='f3 pa3 dim underline pointer black'>Sign Out</p>
-    </nav>
-  );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{display:'flex', justifyContent:'flex-end'}}>
+        <p onClick={() => onRouteChange('signout')} className='f3 pa3 dim underline pointer black'>Sign Out</p>
+      </nav>
+    );
+  }
+  else {
+    return (
+      <div>
+        <nav style={{display:'flex', justifyContent:'flex-end'}}>
+        <p onClick={() => onRouteChange('signin')} className='f3 pa3 dim underline pointer black'>Sign in</p>
+        </nav>
+      </div>
+    )
+  }
 }
 
 export default Navigation;
