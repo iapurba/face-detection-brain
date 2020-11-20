82a11019455c77568f2675ea18031c301c08a58c
Apurba Panja <apurbapanja1@gmail.com>
2020-11-20 15:46:13 +0530

    Add css class name to the parent div

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import brain from './brain.png'
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
+    <div className='logo ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 120, width: 120 }} >
         <div className="Tilt-inner">
           <img className='pa2' src={brain} alt='brain-logo' />
