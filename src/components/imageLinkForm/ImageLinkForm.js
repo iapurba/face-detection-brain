f67403c5e382dbe555d877b7f8d003c2b2c3350b
Apurba Panja <apurbapanja1@gmail.com>
2020-11-23 11:48:19 +0530

    Change style to ImageLinkForm component UI

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onImageSubmit }) => {
   return (
     <div>
-      <p>{'This magic brain will detect faces in your picture. Give it a try.'}</p>
+      <p className='gray'>{'This magic brain will detect faces in your picture. Give it a try.'}</p>
       <div className="center">
         <div className='center form pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
           <button
             className='w-30 grow f4 link pv2 dib white bg-light-purple'
             type='button'
-            onClick={onButtonSubmit}
+            onClick={onImageSubmit}
           >Detect</button>
         </div>
       </div>
