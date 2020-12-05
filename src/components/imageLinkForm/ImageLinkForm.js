6cd36749fc782dd281787752cea1527b84517bbc
Apurba Panja <apurbapanja1@gmail.com>
2020-12-05 21:03:18 +0530

    Updated Image Link form

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -8,7 +8,12 @@ const ImageLinkForm = ({ onInputChange, onImageSubmit }) => {
       <p className='gray'>{'This magic brain will detect faces in your picture. Give it a try.'}</p>
       <div className="center">
         <div className='center form pa4 br3 shadow-5'>
-          <input className='image-url-field f4 pa2 pl3 w-70 center' type='text' onChange={onInputChange} />
+          <input
+            className='image-url-field f4 pa2 pl3 w-70 center'
+            type='text'
+            placeholder='Insert an image url here'
+            onChange={onInputChange}
+          />
           <button
             className='w-30 grow f4 link pv2 dib white bg-light-purple'
             type='button'
